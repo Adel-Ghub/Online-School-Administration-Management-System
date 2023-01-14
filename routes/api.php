03adf8773547628a3bc100f5ca6073708ddb3f07
AdelN94 <adel.n@totersapp.com>
2023-01-14 14:17:49 +0200

    configuring CourseRepository +CourseController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\CourseController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,11 +21,21 @@ use App\Http\Controllers\SchoolController;
 // Route::get('schools/{id}', 'SchoolController@show');
 // Route::put('schools/{id}', 'SchoolController@update');
 // Route::delete('schools/{id}', 'SchoolController@destroy');
+
 Route::get('/schools',[SchoolController::class,'index']);
 Route::post('/schools',[SchoolController::class,'store']);
 Route::put('/schools/{id}',[SchoolController::class,'update']);
 Route::get('/schools/{id}',[SchoolController::class,'show']);
 Route::delete('schools/{id}',[SchoolController::class,'destroy']);
+
+
+
+Route::post('/Courses',[CourseController::class,'store']);
+Route::put('/Courses/{id}',[CrouseController::class,'update']);
+Route::get('/Courses',[CourseController::class,'index']);
+Route::get('/Courses/{id}',[CourseController::class,'show']);
+Route::delete('Courses/{id}',[CourseController::class,'destroy']);
+
 
 
 //Route::get('/schools', 'App\Http\Controllers\SchoolController@index');
